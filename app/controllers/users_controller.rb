4297527b5d858a72d914ec703455b74a8903416b
Felix Sperling <f.sperling@gmx.de>
2014-08-18 09:14:16 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
+require 'sessions_helper'
+
 class UsersController < ApplicationController
+
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :signed_in_malicious_user, only: [:new, :create]
   before_action :correct_user,   only: [:edit, :update]
@@ -14,6 +17,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def edit
@@ -61,13 +65,7 @@ class UsersController < ApplicationController
     
     # Before filters
   
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
-      end
-    end
-    
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
